9016af4975cdb0607b4e050a67e76b75af43c8e5
Erik Antunes <erik.antunes-ext@viavarejo.com.br>
2021-09-14 12:59:08 -0300

    Retrofit e Glide funcionando, falta refatorar em MVVM

--- iddog/app/src/main/java/co/idwall/iddog/presentation/feed/FeedFragment.kt
+++ iddog/app/src/main/java/co/idwall/iddog/presentation/feed/FeedFragment.kt
@@ -2,14 +2,27 @@ package co.idwall.iddog.presentation.feed
 
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewManager
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import co.idwall.iddog.R
 import co.idwall.iddog.databinding.FragmentFeedBinding
+import co.idwall.iddog.domain.ApiRequest
+import co.idwall.iddog.domain.BASE_URL
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_feed.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.Exception
 
 class FeedFragment : Fragment(R.layout.fragment_feed) {
 
@@ -20,6 +33,7 @@ class FeedFragment : Fragment(R.layout.fragment_feed) {
         binding = FragmentFeedBinding.inflate(layoutInflater)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
+        makeApiRequest()
         backgroundAnimation()
 
         binding.refreshDogActionButton.setOnClickListener {
@@ -42,11 +56,36 @@ class FeedFragment : Fragment(R.layout.fragment_feed) {
 
     private fun makeApiRequest() {
 
+        val api = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiRequest::class.java)
+
+        GlobalScope.launch(Dispatchers.IO){
+
+            try {
+
+                val response = api.getRandomDog()
+                Log.d("Main","Size: "+ response.fileSizeBytes.toString())
+
+                if (response.fileSizeBytes < 400_000){
+                    withContext(Dispatchers.Main){
+                        Glide.with(requireContext()).load(response.url).into(binding.ivRandomDog)
+                        binding.ivRandomDog.visibility = View.VISIBLE
+                    }
+                } else {
+                    makeApiRequest()
+                }
+            } catch (e: Exception) {
+                Log.e("Main","Error: " + e.message)
+            }
+        }
+
     }
 
     private fun backgroundAnimation() {
         val animationDrawable: AnimationDrawable = binding.rlLayout.background as AnimationDrawable
-
         animationDrawable.apply {
             setEnterFadeDuration(1000)
             setExitFadeDuration(3000)
